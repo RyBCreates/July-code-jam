04200b39b52ae2398c1c397e43a997555e694d5b
FaridaNelson <faridanelson@gmail.com>
2025-07-18 16:07:20 +0400

    added the logo.png file to the src/assets/images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import "./header.css";
+import logo from "../../assets/images/logo.png";
 
 function Header() {
   return (
     <header className="hero">
       <div className="hero__content">
-        <h1>Plan Your Ultimate Colorado Adventure</h1>
-        <p>Pick your sports. We’ll map your adrenaline-filled route.</p>
-        <button className="btn-primary">Start Planning</button>
+        <div className="hero__logo">
+          <img src={logo} alt="ColoRUSH Logo" className="hero__logo-img" />
+        </div>
+        <div className="hero__tagline">
+          <h1>Plan Your Ultimate Colorado Adventure</h1>
+          <p>Pick your sports. We’ll map your adrenaline-filled route.</p>
+          <button className="btn-primary">Start Planning</button>
+        </div>
       </div>
     </header>
   );
