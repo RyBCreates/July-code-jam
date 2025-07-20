515a856aaf737641df1ca8975e90631a4340a393
Matthews <matthewsjordao@gmail.com>
2025-07-20 16:39:34 -0400

    desgin changes to testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,23 +1,36 @@
 import "./testimonials.css";
+import TestimonialPhoto from "../../assets/images/Example-Photo.jpg";
 
 function Testimonials() {
   return (
     <section className="testimonials">
       <h2>What Our Users Say</h2>
-      <div className="testimonial-cards">
-        <article className="testimonial">
+      <div className="testimonials-cards">
+        <article className="testimonials-card">
+          <img
+            src={TestimonialPhoto}
+            alt="User testimonial"
+          />
           <p>“This app made our trip unforgettable!”</p>
           <p>
             <strong>- Alex, Denver</strong>
           </p>
         </article>
-        <article className="testimonial">
+        <article className="testimonials-card">
+          <img
+            src={TestimonialPhoto}
+            alt="User testimonial"
+          ></img>
           <p>“We hit all the right spots with no stress. Love it!”</p>
           <p>
             <strong>- Morgan, Chicago</strong>
           </p>
         </article>
-        <article className="testimonial">
+        <article className="testimonials-card">
+          <img
+            src={TestimonialPhoto}
+            alt="User testimonial"
+          ></img>
           <p>“Easy to use and totally accurate. Highly recommend.”</p>
           <p>
             <strong>- Jamie, Austin</strong>
