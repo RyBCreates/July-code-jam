cf74525c59367c8eb2480eaf01e7471d62106bc6
Matthews <matthewsjordao@gmail.com>
2025-07-20 13:53:54 -0400

    add How it Works Design

--- src/components/HowItWorks/HowItWorks.jsx
+++ src/components/HowItWorks/HowItWorks.jsx
@@ -1,4 +1,7 @@
 import "./HowItWorks.css";
+import tagIcon from "../../assets/icons/tag.svg";
+import worldIcon from "../../assets/icons/world.svg";
+import calendarIcon from "../../assets/icons/calendar.svg";
 
 function HowItWorks() {
   return (
@@ -6,15 +9,17 @@ function HowItWorks() {
       <h2>How It Works</h2>
       <div className="steps">
         <article className="step">
-          <img src="../../assets/icons/tag.svg" alt="Tag Icon" />
+          <img src={tagIcon} alt="Tag Icon" />
           <h3>1. CHOOSE YOUR TAGS</h3>
           <p>Select your preferred sport, skill level, climate, and dates</p>
         </article>
         <article className="step">
+          <img src={worldIcon} alt="World Icon" />
           <h3>2. WE FIND THE BEST MATCHES</h3>
           <p>We match you with today's best and most exciting activities</p>
         </article>
         <article className="step">
+          <img src={calendarIcon} alt="Calendar Icon" />
           <h3>3. GET A PERSONALIZED ITINERARY</h3>
           <p>Customize your trip and save your booking instantly</p>
         </article>
