62e681d87e24ad2b47cf48e17f44d13f264171f7
Ryan B <bombingrun3@gmail.com>
2025-07-22 10:35:58 -0500

    Adjusted some of the testimonials text

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
 import "./testimonials.css";
-import ExamplePhoto from "../../assets/images/Example-Photo.jpg"
+import ExamplePhoto from "../../assets/images/Example-Photo.jpg";
 
 function Testimonials() {
   return (
@@ -7,31 +7,24 @@ function Testimonials() {
       <h2>What Our Users Say</h2>
       <div className="testimonials-cards">
         <article className="testimonials-card">
-          <img
-            src= {ExamplePhoto}
-            alt="User testimonial"
-          />
+          <img src={ExamplePhoto} alt="User testimonial" />
           <p>“This app made our trip unforgettable!”</p>
           <p>
-            <strong>- Alex, Denver</strong>
+            <strong>- Alex, Boston</strong>
           </p>
         </article>
         <article className="testimonials-card">
-          <img
-            src= {ExamplePhoto}
-            alt="User testimonial"
-          />
+          <img src={ExamplePhoto} alt="User testimonial" />
           <p>“We hit all the right spots with no stress. Love it!”</p>
           <p>
             <strong>- Morgan, Chicago</strong>
           </p>
         </article>
         <article className="testimonials-card">
-          <img
-            src= {ExamplePhoto}
-            alt="User testimonial"
-          />
-          <p>“Beginner to use and totally accurate. Highly recommend.”</p>
+          <img src={ExamplePhoto} alt="User testimonial" />
+          <p>
+            “Beginner-friendly to use and totally accurate. Highly recommend.”
+          </p>
           <p>
             <strong>- Jamie, Austin</strong>
           </p>
