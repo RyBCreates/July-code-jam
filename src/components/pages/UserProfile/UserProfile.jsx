a7b4615f8579573266b00e38e99f80edf1803b8f
Ryan B <bombingrun3@gmail.com>
2025-07-22 09:41:25 -0500

    Added Logout function to UserProfile

--- src/components/pages/UserProfile/UserProfile.jsx
+++ src/components/pages/UserProfile/UserProfile.jsx
@@ -1,4 +1,13 @@
-function UserProfile() {
-  return <h1>User Profile</h1>;
+import "./UserProfile.css";
+
+function UserProfile({ handleLogout }) {
+  return (
+    <section className="profile">
+      <h1 className="profile__title">User Profile</h1>
+      <button className="profile__logout-button" onClick={handleLogout}>
+        Log Out
+      </button>
+    </section>
+  );
 }
 export default UserProfile;
