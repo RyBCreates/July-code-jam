ac52683281ed0c72e8aeca10296354580ba16866
Ryan B <bombingrun3@gmail.com>
2025-07-22 10:01:11 -0500

    Added user info to the User Profile page

--- src/components/pages/UserProfile/UserProfile.jsx
+++ src/components/pages/UserProfile/UserProfile.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import CurrentUserContext from "../../../contexts/CurrentUserContext";
 import "./UserProfile.css";
 
 function UserProfile({ handleLogout }) {
+  const { currentUser } = useContext(CurrentUserContext);
+
   return (
     <section className="profile">
-      <h1 className="profile__title">User Profile</h1>
+      <h1 className="profile__title">{`Welcome, ${currentUser?.username} `}</h1>
       <button className="profile__logout-button" onClick={handleLogout}>
         Log Out
       </button>
