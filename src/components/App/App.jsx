1eb3a332a1f9ab1116f9f7431d03d0b458dbc966
Ryan B <bombingrun3@gmail.com>
2025-07-21 22:56:21 -0500

    Started adding current user context

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "../Header/Header";
@@ -16,8 +16,13 @@ import Footer from "../Footer/Footer";
 import LoginModal from "../modals/LoginModal/LoginModal";
 import RegisterModal from "../modals/RegisterModal/RegisterModal";
 
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+
 function App() {
   const [activeModal, setActiveModal] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const { currentUser } = useContext(CurrentUserContext);
 
   // Open Login Modal
   const handleLoginClick = () => {
@@ -65,35 +70,41 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <div className="app__content">
-        <Header
-          handleLoginClick={handleLoginClick}
-          handleRegisterClick={handleRegisterClick}
+    <CurrentUserContext.Provider
+      value={{
+        currentUser,
+      }}
+    >
+      <div className="app">
+        <div className="app__content">
+          <Header
+            handleLoginClick={handleLoginClick}
+            handleRegisterClick={handleRegisterClick}
+          />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/user" element={<UserProfile />} />
+            <Route path="/create-your-adventure" element={<RouteBuilder />} />
+            <Route path="/optimal-route" element={<OptimalRoute />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/questions" element={<Questions />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+        <LoginModal
+          activeModal={activeModal}
+          closeModal={closeModal}
+          handleSwitchModal={handleSwitchModal}
         />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/user" element={<UserProfile />} />
-          <Route path="/create-your-adventure" element={<RouteBuilder />} />
-          <Route path="/optimal-route" element={<OptimalRoute />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/questions" element={<Questions />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
+        <RegisterModal
+          activeModal={activeModal}
+          closeModal={closeModal}
+          handleSwitchModal={handleSwitchModal}
+        />
       </div>
-      <LoginModal
-        activeModal={activeModal}
-        closeModal={closeModal}
-        handleSwitchModal={handleSwitchModal}
-      />
-      <RegisterModal
-        activeModal={activeModal}
-        closeModal={closeModal}
-        handleSwitchModal={handleSwitchModal}
-      />
-    </div>
+    </CurrentUserContext.Provider>
   );
 }
 
