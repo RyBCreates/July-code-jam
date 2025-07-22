a33865c66d02ed78dd6e41a44152dd7fd240752e
Ryan B <bombingrun3@gmail.com>
2025-07-22 12:07:50 -0500

    Deployed the site

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/July-code-jam",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3001,
